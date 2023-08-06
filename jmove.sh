f88dab3e6eea48348b2a49fc61990b28319dc7e4
author <you@example.com>
2023-08-06 12:55:32 +0800

    filter

--- jmove.sh
+++ jmove.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
 
+# ${parameter//pattern/string}使用string替换pattern
 # 假设输入是a/b/c pattern= .*a.*b.*c[^/]*; c为最后一级目录
+# 
+# ${${1}//\//\.*}: 
+#   ${1}: 表示第一个参数
+#   //  : 是语法的分割符
+#   \   : 转义字符
+#   /   : pattern,要替换的字符串
+#   /  : 分割符
+#   \   : 转义字符
+#   .*  : 替换后的字符串, .表示任意字符, *表示任意次数
 pattern=.*${${1}//\//\.*}[^/]*
-
-root=~
+root=($(ls ~))
+cur=$(pwd)
 # 将find的结果转化为数组
-dirs=($(find ${root} ! -path "*.git*" ! -path "*.local*" -type d -iregex ${pattern}))
+cd ~
+dirs=($(find ${root} ! -path "*.git*" -type d -iregex ${pattern}))
+cd ${cur}
 # dirs=($(find ${root} -type d -iregex ${pattern}))
 #忽略.git目录
 
 #如果数组的个数为0则返回失败
 nums=${#dirs[@]}
-
+suffic=none
 #如果数组的个数为1则直接跳转
-if [ ${nums} -lt 2 ]; then
-    cd ${dirs[1]}
+if [ ${nums} -eq 0 ]; then
+    echo "no match path~"
+elif [ ${nums} -eq 1 ]; then
+    cd ~/${dirs[1]}
 else
     for i in $(seq 1 ${nums})
     do
+      if [[ ${dirs[$i]} != ${suffic}* ]];then
         echo "$i: ${dirs[$i]}"
+        suffic=${dirs[$i]} 
+      fi
     done
     echo -n "select your target path: "
     read index
-    cd ${dirs[$index]}
+    cd ~/${dirs[$index]}
 fi
 
